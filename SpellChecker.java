dfab10d1767c73bac68de35447e9ee65b7da9856
Esther Livingstone <esther.livingstone@post.runi.ac.il>
2024-02-02 20:01:39 +0200

    Hash + SpellChecker

--- SpellChecker.java
+++ SpellChecker.java
@@ -11,25 +11,65 @@ public class SpellChecker {
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+		if (str.length() <= 1) {
+			return "";
+		} else {
+			return str.substring(1);
+		}
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		int m = word1.length();
+    	int n = word2.length();
+
+    	int[][] distanceArray = new int[m + 1][n + 1]; // distance array, to keep the edit distances
+    	for (int i = 0; i <= m; i++) {
+			distanceArray[i][0] = i;
+		}
+    	for (int j = 1; j <= n; j++) {
+        	distanceArray[0][j] = j;
+    	}
+
+    	for (int i = 1; i <= m; i++) {
+        	for (int j = 1; j <= n; j++) {
+            	int substitutions = word1.charAt(i - 1) == word2.charAt(j - 1) ? 0 : 1; // calculates the substitutions 
+            	int minDistance = Math.min(Math.min(distanceArray[i - 1][j] + 1, distanceArray[i][j - 1] + 1), distanceArray[i - 1][j - 1] + substitutions);
+            	distanceArray[i][j] = minDistance;
+        	}
+    	}
+    	return distanceArray[m][n];
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
+		int index = 0;
 
-		// Your code here
-
-		return dictionary;
+    	while (!in.isEmpty() && index < 3000) {
+        	String word = in.readString();
+        	dictionary[index++] = word;
+    	}
+    	in.close();
+    	return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		String closestWord = word;
+		int minDistance = -1;
+
+		for (int i = 0; i < dictionary.length; i++) {
+			String dictWord = dictionary[i];
+			int distance = levenshtein(word, dictWord);
+			if (minDistance == -1 || distance < minDistance) {
+				minDistance = distance;
+				closestWord = dictWord;
+			}
+		}
+		if (minDistance <= threshold) {
+			return closestWord;
+		} else {
+			return word;
+		}
 	}
 
 }
